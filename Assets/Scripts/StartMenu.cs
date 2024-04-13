d717b34bd8c776acab868bb792e3f2904f243ad3
Jonas Piechaczek <jonas.piechaczek@persona-digital.de>
2024-04-13 23:22:13 +0200

    add start screen

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
