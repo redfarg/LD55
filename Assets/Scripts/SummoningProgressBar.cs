1b094585c80c8dbb53019f86f459c49069327470
redfarg <fargred@gmail.com>
2024-04-13 18:05:19 +0200

    add new progress bar

--- Assets/Scripts/SummoningProgressBar.cs
+++ Assets/Scripts/SummoningProgressBar.cs
@@ -7,23 +7,28 @@ using System;
 public class SummoningProgressBar : MonoBehaviour
 {
     [SerializeField] private GameObject summoningProgressBar;
+    [SerializeField] private GameObject summoningProgressBarActive;
+
     private ScoreTracker scoreTracker;
     void Start()
     {
         scoreTracker = GameObject.Find("ScoreTracker").GetComponent<ScoreTracker>();
         scoreTracker.OnSummoningSuccess += UpdateSummoningProgressBar;
         scoreTracker.OnScoreChange += UpdateSummoningProgressBarSize;
+        summoningProgressBar.SetActive(true);
     }
 
     private void UpdateSummoningProgressBarSize(int totalScore)
     {
         Debug.Log("Score: " + totalScore);
         summoningProgressBar.transform.localScale = new Vector3(totalScore / 500f, 1, 1);
+        summoningProgressBarActive.transform.localScale = new Vector3(totalScore / 500f, 1, 1);
     }
 
     private void UpdateSummoningProgressBar()
     {
         Debug.Log("Summoning Success");
-        LeanTween.color(summoningProgressBar, Color.green, 1f);
+        summoningProgressBar.SetActive(false);
+        summoningProgressBarActive.SetActive(true);
     }
 }
