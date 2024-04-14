c6a1bf0ed19efed5404ab92a7135bb44165cc183
Jonas Piechaczek <jonas.piechaczek@persona-digital.de>
2024-04-14 14:50:48 +0200

    implement button press sound for start menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,14 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] private AudioSource buttonPressSound;
     public void StartGame()
     {
+        buttonPressSound.Play();
         Debug.Log("Start Game");
         SceneManager.LoadScene("MainScene");
     }
 
     public void QuitGame()
     {
+        buttonPressSound.Play();
         Debug.Log("Quit Game");
         Application.Quit();
     }
